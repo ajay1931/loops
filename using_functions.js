31903eebf1b1189c95d27303d9420f6b9632c6a9
ajay1931 <ajayprakash032@gmail.com>
2024-06-02 08:22:40 +0530

    function updated

--- using_functions.js
+++ using_functions.js
@@ -22,9 +22,19 @@ function result(tiffen, lunch, dinner) { //inside the bracket called parameters
 result(25, 50, 25) //inside the bracket called arguments
 
 
-function res(tiffen,lunch,dinner){
-   
-    return tiffen+lunch+dinner
+function res(tiffen, lunch, dinner) {//inside the bracket called parameters
+
+    return tiffen + lunch + dinner
 }
-var add=res(50,200,50)
-console.log("total expense: "+add)
+var add = res(50, 200, 50)//inside the bracket called arguments
+console.log("total expense: " + add)
+
+
+function addon() {
+    let total = 0;
+    for (let value of arguments)
+        total += value;
+    return total;
+}
+var call = addon(100, 200, 300, 400)
+console.log("total exp:" + call)
