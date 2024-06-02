38386502c2b0f2fabe6dc33fd2558c3f59e14a42
ajay1931 <ajayprakash032@gmail.com>
2024-06-02 08:09:09 +0530

    update

--- using_functions.js
+++ using_functions.js
@@ -13,8 +13,18 @@ function greetings(name, isMorning) {
 }
 greetings("ram", false)
 
-function result(tiffen,lunch,dinner){
-    let sum=tiffen + lunch + dinner
+
+
+function result(tiffen, lunch, dinner) { //inside the bracket called parameters
+    let sum = tiffen + lunch + dinner
     console.log("total expense:" + sum)
 }
-result(25,50,25)
+result(25, 50, 25) //inside the bracket called arguments
+
+
+function res(tiffen,lunch,dinner){
+   
+    return tiffen+lunch+dinner
+}
+var add=res(50,200,50)
+console.log("total expense: "+add)
