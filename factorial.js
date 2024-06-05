622ad06e0e2d6140f1aa531533770cb6b9edc9f3
ajay1931 <ajayprakash032@gmail.com>
2024-06-05 16:52:35 +0530

    factorial init

---
+++ factorial.js
@@ -0,0 +1,11 @@
+function factorial(n){
+    var multiple=1;
+    for(var i=1;i<=n;i++){
+        multiple =i*multiple;
+        console.log(multiple)
+    }
+    return multiple;
+}
+var n=4;
+var input=factorial(n);
+console.log(input)
