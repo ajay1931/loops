f6fb201efcec17934fdb55fa3e51b555a66ca6c9
ajay1931 <ajayprakash032@gmail.com>
2024-06-07 06:13:29 +0530

    updated

--- factors.js
+++ factors.js
@@ -1,3 +1,17 @@
+// function factorNumber(num,limit) {
+//     var factor = 0;
+//     for (var i = 1; i <=limit; i++) {
+//         factor = i*num;
+//         if(factor<=limit){
+            
+//             console.log(factor)
+//         }
+//     }
+    
+// }
+// factorNumber(20,50000)
+
+
 function factorNumber(num,limit) {
     var factor = 0;
     for (var i = 1; i <=limit; i++) {
